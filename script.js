60d9ea8533623a99cc82a9bde347512d8a94d761
Tumworobere <annaht.annah@gmail.com>
2024-01-05 16:01:59 +0400

    Implement inputDigit and inputDecimal functions to handle user input of digits and decimals

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const calculator = {
     waitingForSecondOperand: false,
     operator: null,
   };
+
+  function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+  }
+
+  function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = "0."
+      calculator.waitingForSecondOperand = false;
+      return
+    }
+
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+  }
