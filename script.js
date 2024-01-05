ad34cdea7078e2c49eb2b76e4dde71037562f8ed
Tumworobere <annaht.annah@gmail.com>
2024-01-05 16:05:14 +0400

    Implement handleOperator function to manage operator input and perform calculations

--- script.js
+++ script.js
@@ -27,3 +27,40 @@ const calculator = {
       calculator.displayValue += dot;
     }
   }
+
+  function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      return;
+    }
+
+
+    if (firstOperand == null && !isNaN(inputValue)) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+      calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+
+  function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+
+    return secondOperand;
+  }
